f15a5e2990a62964e557364fa53b77000ab2cfa2
Juan Carlos Leone Barreto <jc.leone98@gmail.com>
2024-02-16 17:07:54 -0300

    feat: rename module and add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TitleComponent } from './pages/index/title/title.component';
 
+import { IndexModule } from './pages/index/index.module';
+import { PortifolioModule } from './pages/portfolio/portifolio.module';
+import { MenuComponent } from './shared/menu/menu.component'
+
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PortifolioModule,
+    IndexModule
   ],
   providers: [],
   bootstrap: [AppComponent]
